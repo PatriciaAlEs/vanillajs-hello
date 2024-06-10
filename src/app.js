974752e63a175e2a5eff84d23e337cec87db728b
PatriciaAlEs <168550797+PatriciaAlEs@users.noreply.github.com>
2024-06-10 14:53:12 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,36 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generandoExcusas(quien, accion, que, cuando) {
+  let itemQuien = Math.floor(Math.random() * quien.length);
+  let itemAccion = Math.floor(Math.random() * accion.length);
+  let itemQue = Math.floor(Math.random() * que.length);
+  let itemCuando = Math.floor(Math.random() * cuando.length);
+
+  return (
+    quien[itemQuien] +
+    " " +
+    accion[itemAccion] +
+    " " +
+    que[itemQue] +
+    " " +
+    cuando[itemCuando]
+  );
+}
+
+function onLoad() {
+  var excusa = document.getElementById("excusas");
+  excusa.innerHTML = generandoExcusas(who, action, what, when);
+}
+window.onload = onLoad;
